4d0419fdd6a15b794c7937b25dc6fcd8a79b0ada
Omar Ahmed <omarking05@gmail.com>
2022-02-22 22:28:43 +0200

    feat(client): split code into functions and move it to main

--- main.go
+++ main.go
@@ -4,22 +4,46 @@ import (
 	"bufio"
 	"log"
 	"os"
+	"os/signal"
 )
 
-func main() {
-  input := make(chan string)
+var interrupt chan os.Signal
 
-  InitClient(input)
+func main() {
+	interrupt = make(chan os.Signal) // Channel to listen for interrupt signal to gracefully terminate
+	input := make(chan string)
+
+	signal.Notify(interrupt, os.Interrupt)
+
+	connection := InitClient(input)
+	defer connection.Close()
+
+  go ScanFile(input)
+
+	// Main loop of the client
+	// Here we send & receive packets
+	for {
+		select {
+		case line := <-input:
+			log.Println(line)
+			SendMessage(connection, line)
+
+		case <-interrupt:
+			log.Println("Received SIGINT interrupt signal. Closing all pending connections")
+      HandleWebsocketClose(connection)
+			return
+		}
+	}
 }
 
 func ScanFile(input chan string) {
-  scanner := bufio.NewScanner(os.Stdin)
-  for scanner.Scan() {
-    text := scanner.Text()
-    input <- text
-  }
+	scanner := bufio.NewScanner(os.Stdin)
+	for scanner.Scan() {
+		text := scanner.Text()
+		input <- text
+	}
 
-  if err := scanner.Err(); err != nil {
-    log.Println(err)
-  }
+	if err := scanner.Err(); err != nil {
+		log.Println(err)
+	}
 }
