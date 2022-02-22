7f49d61a2c49d112817322dff733a53665056a77
Omar Ahmed <omarking05@gmail.com>
2022-02-23 00:47:16 +0200

    feat(hub): use client id as map key

--- server/hub.go
+++ server/hub.go
@@ -2,7 +2,7 @@ package main
 
 // Maintain the set of active clients
 type Hub struct {
-	clients    map[*Client]bool
+	clients    map[string]*Client
 	broadcast  chan []byte
 	register   chan *Client
 	unregister chan *Client
@@ -10,7 +10,7 @@ type Hub struct {
 
 func NewHub() *Hub {
 	return &Hub{
-		clients:    make(map[*Client]bool),
+		clients:    make(map[string]*Client),
 		broadcast:  make(chan []byte),
 		register:   make(chan *Client),
 		unregister: make(chan *Client),
@@ -21,18 +21,18 @@ func (h *Hub) Run() {
 	for {
 		select {
 		case client := <-h.register:
-			h.clients[client] = true
+			h.clients[client.id] = client
 		case client := <-h.unregister:
-			if _, ok := h.clients[client]; ok {
-				delete(h.clients, client)
+			if _, ok := h.clients[client.id]; ok {
+				delete(h.clients, client.id)
         close(client.send)
 			}
     case message := <- h.broadcast:
-      for client := range h.clients {
+      for _, client := range h.clients {
         select {
         case client.send <- message:
         default:
-          delete(h.clients, client)
+          delete(h.clients, client.id)
           close(client.send)
         }
       }
