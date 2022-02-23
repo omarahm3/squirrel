1adcc165d78cf8cabe3253ba220f551ab11c11a1
Omar Ahmed <omarking05@gmail.com>
2022-02-23 22:18:38 +0200

    feat(hub): handle client update

--- server/hub.go
+++ server/hub.go
@@ -1,11 +1,17 @@
 package main
 
+import "log"
+
 // Maintain the set of active clients
 type Hub struct {
 	clients    map[string]*Client
 	broadcast  chan []byte
 	register   chan *Client
 	unregister chan *Client
+	update     chan struct {
+		id     string
+		client *Client
+	}
 }
 
 func NewHub() *Hub {
@@ -14,6 +20,21 @@ func NewHub() *Hub {
 		broadcast:  make(chan []byte),
 		register:   make(chan *Client),
 		unregister: make(chan *Client),
+		update: make(chan struct {
+			id     string
+			client *Client
+		}),
+	}
+}
+
+// removeChannel is a flag must be set to true to remove client send channel
+// This flag is added to avoid removing client channel when updating the client
+func removeClient(hub *Hub, clientId string, removeChannel bool) {
+	if client, ok := hub.clients[clientId]; ok {
+		delete(hub.clients, clientId)
+		if removeChannel {
+			close(client.send)
+		}
 	}
 }
 
@@ -21,12 +42,15 @@ func (h *Hub) Run() {
 	for {
 		select {
 		case client := <-h.register:
+			log.Println("Registering client:", client.id)
 			h.clients[client.id] = client
+		case info := <-h.update:
+			log.Printf("Updating client ID: [%s] to [%s]", info.id, info.client.id)
+			removeClient(h, info.id, false)
+			h.clients[info.client.id] = info.client
+
 		case client := <-h.unregister:
-			if _, ok := h.clients[client.id]; ok {
-				delete(h.clients, client.id)
-				close(client.send)
-			}
+			removeClient(client.hub, client.id, true)
 		case message := <-h.broadcast:
 			for _, client := range h.clients {
 				select {
