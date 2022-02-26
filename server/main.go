9b5ecb6fb36d5bca4a9ffb231c40c790bc49e85c
Omar Ahmed <omarking05@gmail.com>
2022-02-26 03:39:52 +0200

    feat(server): pass domain to view

--- server/main.go
+++ server/main.go
@@ -7,7 +7,7 @@ import (
 
 	"github.com/gin-gonic/gin"
 	"github.com/gorilla/websocket"
-  "github.com/omarahm3/squirrel/utils"
+	"github.com/omarahm3/squirrel/utils"
 	"go.uber.org/zap"
 )
 
@@ -17,16 +17,16 @@ var wsUpgrader = websocket.Upgrader{
 }
 
 func wsHandler(hub *Hub, r *http.Request, w http.ResponseWriter) {
-  zap.S().Info("Handling websocket upgrade request")
+	zap.S().Info("Handling websocket upgrade request")
 
 	connection, err := wsUpgrader.Upgrade(w, r, nil)
 
 	if err != nil {
-    zap.L().Error("Error upgrading websocket request, ignoring", zap.Error(err))
+		zap.L().Error("Error upgrading websocket request, ignoring", zap.Error(err))
 		return
 	}
 
-  zap.S().Info("Websocket connection was successful")
+	zap.S().Info("Websocket connection was successful")
 
 	client := &Client{
 		id:         utils.GenerateUUID(),
@@ -36,67 +36,67 @@ func wsHandler(hub *Hub, r *http.Request, w http.ResponseWriter) {
 		send:       make(chan []byte, 256),
 	}
 
-  zap.S().Infow("Initialized new client", "clientId", client.id)
+	zap.S().Infow("Initialized new client", "clientId", client.id)
 
 	client.hub.register <- client
 
 	go client.ReadPump()
-  go client.WritePump()
+	go client.WritePump()
 }
 
 func main() {
-  if utils.GetEnv() != "dev" {
-    gin.SetMode(gin.ReleaseMode)
-  }
+	if utils.GetEnv() != "dev" {
+		gin.SetMode(gin.ReleaseMode)
+	}
 
-  utils.InitLogging()
+	utils.InitLogging()
 
-  defer func() {
-    _ = zap.L().Sync()
-    _ = zap.S().Sync()
-  }()
+	defer func() {
+		_ = zap.L().Sync()
+		_ = zap.S().Sync()
+	}()
 
-  envPort := utils.GetEnvVariable("PORT")
+	envPort := utils.GetEnvVariable("PORT")
 
-  if envPort == "" {
-    envPort = "3000"
-  }
+	if envPort == "" {
+		envPort = "3000"
+	}
 
-  port, err := strconv.Atoi(envPort)
+	port, err := strconv.Atoi(envPort)
 
-  if err != nil {
-    utils.FatalError("Couldn't convert port to int", err)
-  }
+	if err != nil {
+		utils.FatalError("Couldn't convert port to int", err)
+	}
 
 	server := gin.Default()
 
-  zap.S().Debug("Prepared server default")
+	zap.S().Debug("Prepared server default")
 
 	hub := NewHub()
 
-  zap.S().Debug("Created clients hub")
+	zap.S().Debug("Created clients hub")
 
 	go hub.Run()
 
-  zap.S().Debug("Loading server HTML files")
+	zap.S().Debug("Loading server HTML files")
 
 	server.LoadHTMLFiles("./view/index.html")
 
-  initRoutes(server, hub)
+	initRoutes(server, hub)
 
-  zap.S().Debugf("Running server on port [%d]\n", port)
+	zap.S().Debugf("Running server on port [%d]\n", port)
 
-  err = server.Run(fmt.Sprintf(":%d", port))
+	err = server.Run(fmt.Sprintf(":%d", port))
 
 	if err != nil {
-    utils.FatalError("Error while running server", err)
+		utils.FatalError("Error while running server", err)
 	}
 
 	fmt.Printf("Server is running on http://localhost:%d", port)
 }
 
 func initRoutes(server *gin.Engine, hub *Hub) {
-  zap.S().Debug("Initializing server routes")
+	zap.S().Debug("Initializing server routes")
 
 	server.GET("/", func(context *gin.Context) {
 		context.HTML(200, "index.html", nil)
@@ -109,24 +109,25 @@ func initRoutes(server *gin.Engine, hub *Hub) {
 	server.GET("/client/:clientId", func(context *gin.Context) {
 		clientId := context.Param("clientId")
 
-    zap.S().Debugf("Incoming request to subscribe to client ID: [%s]\n", clientId)
+		zap.S().Debugf("Incoming request to subscribe to client ID: [%s]\n", clientId)
 
 		if clientId == "" {
-      zap.S().Debug("Client ID is empty ignoring")
+			zap.S().Debug("Client ID is empty ignoring")
 			context.String(400, "Cannot be empty")
 			return
 		}
 
 		if _, ok := hub.clients[clientId]; !ok {
-      zap.S().Debugf("Client ID: [%s] doesn't exist on the hub\n", clientId)
+			zap.S().Debugf("Client ID: [%s] doesn't exist on the hub\n", clientId)
 			context.String(404, "Client not found")
 			return
 		}
 
-    zap.S().Debugf("Client ID: [%s] was found on hub\n", clientId)
+		zap.S().Debugf("Client ID: [%s] was found on hub\n", clientId)
 
 		context.HTML(200, "index.html", gin.H{
 			"clientId": clientId,
+			"domain":   utils.GetEnvVariable("DOMAIN"),
 		})
 	})
 }
