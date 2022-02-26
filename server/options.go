50db556e61b4405b142400dc49575fdd120524d2
Omar Ahmed <omarking05@gmail.com>
2022-02-27 01:55:25 +0200

    feat(server): share options code with utils

--- server/options.go
+++ server/options.go
@@ -4,10 +4,8 @@ import (
 	"flag"
 	"fmt"
 	"os"
-	"strconv"
 
 	"github.com/omarahm3/squirrel/utils"
-	"go.uber.org/zap"
 	"go.uber.org/zap/zapcore"
 )
 
@@ -33,35 +31,6 @@ func fprintf(format string, a ...interface{}) {
 	fmt.Fprintf(os.Stderr, format, a...)
 }
 
-func winningDefault(envVariable string, defaultVariable string) string {
-	if envVariable == "" {
-		return defaultVariable
-	}
-	return envVariable
-}
-
-func toInt(value string) int {
-	intVal, err := strconv.Atoi(value)
-
-	if err != nil {
-		fmt.Println("Error converting value to int", err)
-		os.Exit(1)
-	}
-
-	return intVal
-}
-
-func handleLogging(loglevel string) zapcore.Level {
-	level, err := zapcore.ParseLevel(loglevel)
-
-	if err != nil {
-		fmt.Println("Invalid log level returned, setting default log level, Error: ", err)
-		level = zap.ErrorLevel
-	}
-
-	return level
-}
-
 func InitOptions() *ServerOptions {
 	flag.Usage = func() {
 		fprintf("Usage of %s:\n", os.Args[0])
@@ -80,20 +49,20 @@ func InitOptions() *ServerOptions {
 		writeBufferSize string = utils.GetEnvVariable("WRITE_BUFFER_SIZE")
 	)
 
-	flag.StringVar(&env, "env", winningDefault(utils.GetEnvVariable("APP_ENV"), DEFAULT_ENVIRONMENT), "Server environment (prod|dev)")
-	flag.StringVar(&domain, "domain", winningDefault(utils.GetEnvVariable("DOMAIN"), DEFAULT_DOMAIN), "Server domain")
-	flag.StringVar(&loglevel, "log", winningDefault(utils.GetEnvVariable("LOG_LEVEL"), DEFAULT_LOG_LEVEL), "Log level")
-	flag.StringVar(&port, "port", winningDefault(utils.GetEnvVariable("PORT"), DEFAULT_PORT), "Server port")
-	flag.StringVar(&readBufferSize, "read-buffer-size", winningDefault(utils.GetEnvVariable("READ_BUFFER_SIZE"), DEFAULT_READ_BUFFER_SIZE), "Websocket read buffer size")
-	flag.StringVar(&writeBufferSize, "write-buffer-size", winningDefault(utils.GetEnvVariable("WRITE_BUFFER_SIZE"), DEFAULT_WRITE_BUFFER_SIZE), "Websocket write buffer size")
+	flag.StringVar(&env, "env", utils.WinningDefault(utils.GetEnvVariable("APP_ENV"), DEFAULT_ENVIRONMENT), "Server environment (prod|dev)")
+	flag.StringVar(&domain, "domain", utils.WinningDefault(utils.GetEnvVariable("DOMAIN"), DEFAULT_DOMAIN), "Server domain")
+	flag.StringVar(&loglevel, "log", utils.WinningDefault(utils.GetEnvVariable("LOG_LEVEL"), DEFAULT_LOG_LEVEL), "Log level")
+	flag.StringVar(&port, "port", utils.WinningDefault(utils.GetEnvVariable("PORT"), DEFAULT_PORT), "Server port")
+	flag.StringVar(&readBufferSize, "read-buffer-size", utils.WinningDefault(utils.GetEnvVariable("READ_BUFFER_SIZE"), DEFAULT_READ_BUFFER_SIZE), "Websocket read buffer size")
+	flag.StringVar(&writeBufferSize, "write-buffer-size", utils.WinningDefault(utils.GetEnvVariable("WRITE_BUFFER_SIZE"), DEFAULT_WRITE_BUFFER_SIZE), "Websocket write buffer size")
 	flag.Parse()
 
 	return &ServerOptions{
 		Env:             env,
 		Domain:          domain,
-		Port:            toInt(port),
-		LogLevel:        handleLogging(loglevel),
-		ReadBufferSize:  toInt(readBufferSize),
-		WriteBufferSize: toInt(writeBufferSize),
+		Port:            utils.StrToInt(port),
+		LogLevel:        utils.GetLogLevelFromString(loglevel),
+		ReadBufferSize:  utils.StrToInt(readBufferSize),
+		WriteBufferSize: utils.StrToInt(writeBufferSize),
 	}
 }
