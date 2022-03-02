0a8afda36a827e368d2f17c31769a3f599f0dfe2
Omar Ahmed <omarking05@gmail.com>
2022-03-02 23:45:01 +0200

    refactor(server): move http method to separate file

---
+++ server/http.go
@@ -0,0 +1,82 @@
+package server
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	"github.com/gorilla/websocket"
+	"github.com/omarahm3/squirrel/utils"
+	"go.uber.org/zap"
+)
+
+func InitHttpServer() {
+	zap.S().Debug("Initializing server routes")
+
+	server.GET("/", func(context *gin.Context) {
+		context.HTML(200, "index.html", nil)
+	})
+
+	server.GET("/ws", func(context *gin.Context) {
+		WebsocketHandler(context.Request, context.Writer)
+	})
+
+	server.GET("/client/:clientId", SubscriberView)
+}
+
+func WebsocketHandler(r *http.Request, w http.ResponseWriter) {
+	zap.S().Info("Handling websocket upgrade request")
+
+	var wsUpgrader = websocket.Upgrader{
+		ReadBufferSize:  options.ReadBufferSize,
+		WriteBufferSize: options.WriteBufferSize,
+	}
+
+	connection, err := wsUpgrader.Upgrade(w, r, nil)
+
+	if err != nil {
+		zap.L().Error("Error upgrading websocket request, ignoring", zap.Error(err))
+		return
+	}
+
+	zap.S().Info("Websocket connection was successful")
+
+	client := &Client{
+		id:          utils.GenerateUUID(),
+		connection:  connection,
+		hub:         hub,
+		broadcaster: false,
+		send:        make(chan []byte, 256),
+	}
+
+	zap.S().Infow("Initialized new client", "clientId", client.id)
+
+	client.hub.register <- client
+
+	go client.ReadPump()
+	go client.WritePump()
+}
+
+func SubscriberView(context *gin.Context) {
+	clientId := context.Param("clientId")
+
+	zap.S().Debugf("Incoming request to subscribe to client ID: [%s]\n", clientId)
+
+	if clientId == "" {
+		zap.S().Debug("Client ID is empty ignoring")
+		context.String(400, "Cannot be empty")
+		return
+	}
+
+	if _, ok := hub.clients[clientId]; !ok {
+		zap.S().Debugf("Client ID: [%s] doesn't exist on the hub\n", clientId)
+		context.String(404, "Client not found")
+		return
+	}
+
+	zap.S().Debugf("Client ID: [%s] was found on hub\n", clientId)
+
+	context.HTML(200, "index.html", gin.H{
+		"clientId": clientId,
+		"domain":   options.Domain.Websocket,
+	})
+}
--- server/main.go
+++ server/main.go
@@ -2,48 +2,17 @@ package server
 
 import (
 	"fmt"
-	"net/http"
 
 	"github.com/gin-gonic/gin"
-	"github.com/gorilla/websocket"
 	"github.com/omarahm3/squirrel/utils"
 	"go.uber.org/zap"
 )
 
-var options *ServerOptions
-
-func wsHandler(hub *Hub, r *http.Request, w http.ResponseWriter) {
-	zap.S().Info("Handling websocket upgrade request")
-
-	var wsUpgrader = websocket.Upgrader{
-		ReadBufferSize:  options.ReadBufferSize,
-		WriteBufferSize: options.WriteBufferSize,
-	}
-
-	connection, err := wsUpgrader.Upgrade(w, r, nil)
-
-	if err != nil {
-		zap.L().Error("Error upgrading websocket request, ignoring", zap.Error(err))
-		return
-	}
-
-	zap.S().Info("Websocket connection was successful")
-
-	client := &Client{
-		id:          utils.GenerateUUID(),
-		connection:  connection,
-		hub:         hub,
-		broadcaster: false,
-		send:        make(chan []byte, 256),
-	}
-
-	zap.S().Infow("Initialized new client", "clientId", client.id)
-
-	client.hub.register <- client
-
-	go client.ReadPump()
-	go client.WritePump()
-}
+var (
+	options *ServerOptions
+	hub     *Hub
+	server  *gin.Engine
+)
 
 func printOptions() {
 	zap.S().Warnw(
@@ -79,11 +48,11 @@ func Main() {
 		_ = zap.S().Sync()
 	}()
 
-	server := gin.Default()
+	server = gin.Default()
 
 	zap.S().Debug("Prepared server default")
 
-	hub := NewHub()
+	hub = NewHub()
 
 	zap.S().Debug("Created clients hub")
 
@@ -93,7 +62,7 @@ func Main() {
 
 	server.LoadHTMLFiles("./server/view/index.html")
 
-	initRoutes(server, hub)
+	InitHttpServer()
 
 	zap.S().Debugf("Running server on port [%d]\n", options.Port)
 
@@ -104,41 +73,4 @@ func Main() {
 	}
 
 	fmt.Printf("Server is running on http://localhost:%d", options.Port)
-}
-
-func initRoutes(server *gin.Engine, hub *Hub) {
-	zap.S().Debug("Initializing server routes")
-
-	server.GET("/", func(context *gin.Context) {
-		context.HTML(200, "index.html", nil)
-	})
-
-	server.GET("/ws", func(context *gin.Context) {
-		wsHandler(hub, context.Request, context.Writer)
-	})
-
-	server.GET("/client/:clientId", func(context *gin.Context) {
-		clientId := context.Param("clientId")
-
-		zap.S().Debugf("Incoming request to subscribe to client ID: [%s]\n", clientId)
-
-		if clientId == "" {
-			zap.S().Debug("Client ID is empty ignoring")
-			context.String(400, "Cannot be empty")
-			return
-		}
-
-		if _, ok := hub.clients[clientId]; !ok {
-			zap.S().Debugf("Client ID: [%s] doesn't exist on the hub\n", clientId)
-			context.String(404, "Client not found")
-			return
-		}
-
-		zap.S().Debugf("Client ID: [%s] was found on hub\n", clientId)
-
-		context.HTML(200, "index.html", gin.H{
-			"clientId": clientId,
-			"domain":   options.Domain.Websocket,
-		})
-	})
 }
