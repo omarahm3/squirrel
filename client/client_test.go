7fe0636e07f27fdad8ac0a78d071d1b62805df94
Omar Ahmed <omarking05@gmail.com>
2022-04-03 23:35:04 +0200

    feat(client): test sending subscriber ack message

--- client/client_test.go
+++ client/client_test.go
@@ -11,57 +11,99 @@ import (
 )
 
 func echo(w http.ResponseWriter, r *http.Request) {
-  connection, err := websocket.Upgrade(w, r, nil, 0, 0)
+	connection, err := websocket.Upgrade(w, r, nil, 0, 0)
 
-  if err != nil {
-    return
-  }
+	if err != nil {
+		return
+	}
 
-  defer connection.Close()
+	defer connection.Close()
 
-  for {
-    messageType, message, err := connection.ReadMessage()
+	for {
+		messageType, message, err := connection.ReadMessage()
 
-    if err != nil {
-      break
-    }
-    
-    err = connection.WriteMessage(messageType, message)
+		if err != nil {
+			break
+		}
 
-    if err != nil {
-      break
-    }
-  }
+		err = connection.WriteMessage(messageType, message)
+
+		if err != nil {
+			break
+		}
+	}
 }
 
 func TestInitClient(t *testing.T) {
-  defer ResetTesting(nil)
-  options = InitOptions()
+	defer ResetTesting(nil)
+	options = InitOptions()
 
-  server := httptest.NewServer(http.HandlerFunc(echo))
-  defer server.Close()
+	server := newServer()
+	defer server.Close()
 
-  url := strings.TrimPrefix(server.URL, "http://")
+	overrideClientOptionsDomain(server)
+
+	connection := InitClient()
+	defer connection.Close()
+
+	expected := "test"
+
+	if err := connection.WriteMessage(websocket.TextMessage, []byte(expected)); err != nil {
+		t.Fatal("error sending text message", err)
+	}
+
+	_, message, err := connection.ReadMessage()
+
+	if err != nil {
+		t.Fatal("error reading message", err)
+	}
+
+	if string(message) != expected {
+		t.Errorf("expected %q, actual %q", expected, string(message))
+	}
+}
+
+func TestClientSubscriberAckMessage(t *testing.T) {
+  events  = make(chan string)
   
-  // override the global options and use server URL
-  options.Domain = common.BuildDomain(url, "dev")
+	defer ResetTesting(nil)
+	options = InitOptions()
+
+	server := newServer()
+	defer server.Close()
+
+	overrideClientOptionsDomain(server)
+
+	connection := InitClient()
+	defer connection.Close()
+
+	jsonMessage := common.Message{
+		Id:    clientId,
+		Event: EVENT_SUBSCRIBER_ACK,
+		Payload: common.SubscriberConnectedMessage{
+      Connected: true,
+		},
+	}
+
+  connection.WriteJSON(jsonMessage)
+
+  go HandleIncomingMessages(connection)
   
-  connection := InitClient()
-  defer connection.Close()
-  
-  expected := "test"
-  
-  if err := connection.WriteMessage(websocket.TextMessage, []byte(expected)); err != nil {
-    t.Fatal("error sending text message", err)
+  // Subscriber must be connected so that we receive message on events channel
+  event := <-events
+
+  if event != jsonMessage.Event {
+    t.Errorf("expected %q, actual %q", jsonMessage.Event, event)
   }
+}
 
-  _, message, err := connection.ReadMessage()
+func overrideClientOptionsDomain(server *httptest.Server) {
+	url := strings.TrimPrefix(server.URL, "http://")
 
-  if err != nil {
-    t.Fatal("error reading message", err)
-  }
+	// override the global options and use server URL
+	options.Domain = common.BuildDomain(url, "dev")
+}
 
-  if string(message) != expected {
-    t.Errorf("expected %q, actual %q", expected, string(message))
-  }
+func newServer() *httptest.Server {
+	return httptest.NewServer(http.HandlerFunc(echo))
 }
